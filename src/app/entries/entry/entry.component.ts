db1b6a14f3c841e682f1ed4e6f42e97fb454062b
Zac Laudick <zlaudick@nmsu.edu>
2017-03-20 19:20:51 -0600

    angular services

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Entry } from '../shared/entry.model';
 
 @Component({
     selector: 'app-entry',
@@ -7,12 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class EntryComponent {
-    title: string = 'My first photo';
-    photo: string = 'http://placehold.it/800x500?text=Angular Basics';
-    description: string = 'A description of my first photo';
-    comments: any[] = [
-        {name: "John", comment: "A comment"},
-        {name: "Jim", comment: "A comment"},
-        {name: "Jen", comment: "A comment"}
-    ]
+    @Input() entry: Entry;
 }
