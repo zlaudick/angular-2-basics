e5baacfd698cb53f7ed61696be54638f104c2625
Zac Laudick <zlaudick@nmsu.edu>
2017-03-21 22:01:40 -0600

    angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent } from './entries';
 import { InMemoryEntryService } from './backend';
 
 @NgModule({
     imports: [
         BrowserModule,
         HttpModule,
+        FormsModule,
         InMemoryWebApiModule.forRoot(InMemoryEntryService)
     ],
     providers: [ EntryService ],
     declarations: [
         AppComponent,
         EntryComponent,
-        EntryListComponent
+        EntryListComponent,
+        EntryCommentFormComponent
     ],
     bootstrap: [AppComponent]
 })
